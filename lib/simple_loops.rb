1fcdd2122eb8d636c03eebc5f67effe30c3340d8
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-31 03:03:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,6 +20,14 @@ def output_array(array)
 end
 
 def return_string_array(array)
+  array_of_strings = []
+  counter = 0 
+  while counter < array.length do 
+    array_of_strings.push(array[counter].to_s)
+    counter += 1 
+  end 
+  return array_of_strings
+end 
 
 
 
