610cbbdba00eeded5f8ff72b0ccc4f3f3a5f8cfb
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-31 02:58:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+def loop_message_five_times(message)
+  5.times do 
+    puts message 
+  end 
+end
+
+def loop_message_n_times(message, int)
+  n = int 
+  n.times do 
+    puts message 
+  end
+end
+
+def output_array(array)
+  i = 0 
+  while i < array.count do 
+    puts array[i]
+  end
+end
+
